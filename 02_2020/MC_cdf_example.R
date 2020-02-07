b72b7bc3761ccfd8aa4416243c0ad73902574504
Jose Hernandez <jmhernandeze@gmail.com>
2020-02-07 11:12:18 -0800

    ecdf hist simulation

---
+++ 02_2020/MC_cdf_example.R
@@ -0,0 +1,80 @@
+# office hour question 
+
+site_data <- c(
+  660,
+  673,
+  781,
+  578,
+  1414,
+  673,
+  861,
+  727,
+  735,
+  706,
+  871,
+  689,
+  635,
+  615,
+  616,
+  791,
+  896,
+  707,
+  821,
+  1426,
+  966,
+  950,
+  1012,
+  909,
+  726,
+  909,
+  1226,
+  1304,
+  1246,
+  1151,
+  898,
+  837,
+  693,
+  733,
+  1672,
+  734,
+  860,
+  579
+)
+
+# 38 records 
+# calculate CDF (empirical CDF) of the data
+x_cdf = ecdf(site_data)
+
+plot(x_cdf)
+
+# see values that correspond to the probabilities with location 
+sort(x_cdf(site_data))
+sort(site_data) 
+
+# simulate 20 values from the ecdf 100 times 
+sims <- replicate(100, as.numeric(quantile(x_cdf, runif(20))))
+str(sims)
+
+# calculate 100 means 
+sim_means <- apply(sims, 2, mean)
+length(sim_means)
+
+# mean distribution
+hist(sim_means)
+
+# get function for look up using the 100 simulated means 
+e_sim_means <- ecdf(sim_means)
+# look up the value
+as.numeric(quantile(e_sim_means, .975))
+
+# more directly  
+as.numeric(quantile(sort(sim_means), .975))
+
+
+
+
+
+
+
+
+
