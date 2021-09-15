5d63f45071526d3c633617912dfe4f20cff8f288
Jose Hernandez <jmhernandeze@gmail.com>
2021-09-15 14:56:21 -0700

    tokenization using split

--- 08_2021/list_join_example.py
+++ 08_2021/list_join_example.py
@@ -6,3 +6,21 @@ joined_list = list_1 + list_2
 joined_list
 
 unique_list = list(set(joined_list))
+
+# Finding bigrams 
+sentence = """
+Have free hours and love children? 
+Drive kids to school, soccer practice 
+and other activities.
+"""
+# example:
+print(tokenized_text[0],tokenized_text[1])
+
+# create a list of tokens 
+tokenized_text = sentence.split()
+for i in range(len(tokenized_text)):
+    if i < len(tokenized_text)-1:
+        # take each pair of tokens by moving a window of two
+        print((tokenized_text[i], tokenized_text[i+1]))
+
+
